e15a82f1ee6681850fa2035e1092319b130de1ea
Christopher Witt <christopher.witt@gm.com>
2019-12-30 13:13:07 -0500

    Stuff

---
+++ _ios_/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <android_intent/AndroidIntentPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTAndroidIntentPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTAndroidIntentPlugin"]];
+}
+
+@end
---
+++ _ios_/buddybuild_prebuild.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/flutter/flutter.git -b alpha --depth 1
+export FLUTTER_FRAMEWORK_DIR=flutter
